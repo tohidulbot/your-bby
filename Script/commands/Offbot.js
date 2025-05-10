59390ce94f34b6001904c058c21eedc09a12dfa5
tohidulbot <tohidulbot01@gmail.com>
2025-05-10 18:37:44 +0600

    Update Offbot.js

--- Script/commands/Offbot.js
+++ Script/commands/Offbot.js
@@ -9,6 +9,6 @@ module.exports.config = {
         };
 module.exports.run = ({event, api}) =>{
     const permission = ["100092006324917", "100092006324917"];
-  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For ULLASH", event.threadID, event.messageID);
+  	if (!permission.includes(event.senderID)) return api.sendMessage("[☠️🖕 ] TUI PARBI NA AMARE OFF KORETE🖕 TOR ABBARE KO 👦", event.threadID, event.messageID);
   api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
